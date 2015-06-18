4db6554518505056ec435ce0095f27d3f1eda65f
masaibar <orebatemoko@gmail.com>
2015-06-18 22:39:03 +0900

    #7

---
+++ android/fragment/MyFragmentApp/app/src/main/java/jp/co/masaibar/myfragmentapp/SubActivity.java
@@ -0,0 +1,16 @@
+package jp.co.masaibar.myfragmentapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SubActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
