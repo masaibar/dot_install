221785c9148e44cf2943f3a30841d15673e512d4
masaibar <orebatemoko@gmail.com>
2015-06-18 22:49:26 +0900

    #7

---
+++ android/fragment/MyFragmentApp/app/src/main/java/jp/co/masaibar/myfragmentapp/DetailFragment.java
@@ -0,0 +1,36 @@
+package jp.co.masaibar.myfragmentapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import  android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by masaibar on 2015/06/18.
+ */
+public class DetailFragment extends Fragment{
+
+    public DetailFragment() {
+    }
+
+    public static DetailFragment newInstance(int postion) {
+        DetailFragment detailFragment = new DetailFragment();
+        Bundle args = new Bundle();
+        args.putInt("position", postion);
+        detailFragment.setArguments(args);
+
+        return detailFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View detailView = inflater.inflate(R.layout.view_detail, container, false);
+//        return super.onCreateView(inflater, container, savedInstanceState);
+        ((TextView) detailView.findViewById(R.id.detailText))
+                .setText(News.Details[getArguments().getInt("position")]);
+    }
+}
